3ef0184b32ea69a30e84b02572b405ea10e98717
advanced-go <mark.wilson.go@outlook.com>
2024-10-23 06:37:21 -0500

    Initial commit

---
+++ json/reader.go
@@ -0,0 +1,19 @@
+package json
+
+import (
+	"bytes"
+	"github.com/advanced-go/common/core"
+	"io"
+)
+
+// NewReadCloser - create an io.ReadCloser from a type
+func NewReadCloser(v any) (io.ReadCloser, int64, *core.Status) {
+	if v == nil {
+		return io.NopCloser(bytes.NewReader([]byte{})), 0, core.StatusOK()
+	}
+	buf, status := Marshal(v)
+	if !status.OK() {
+		return nil, 0, status
+	}
+	return io.NopCloser(bytes.NewReader(buf)), int64(len(buf)), core.StatusOK()
+}
